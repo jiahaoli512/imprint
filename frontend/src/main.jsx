df0859ed650f80278413dcebf6f458ed9e5518c0
Jiahao Li <jiahaoli512@gmail.com>
2026-06-14 14:50:43 -0700

    Remove unused StatusBar plugin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Capacitor } from '@capacitor/core'
-import { StatusBar, Style } from '@capacitor/status-bar'
 import './index.css'
 import App from './App.jsx'
-
-if (Capacitor.isNativePlatform()) {
-  StatusBar.setOverlaysWebView({ overlay: false });
-  StatusBar.setStyle({ style: Style.Dark });
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
